562f3c91e362866fc851b9805782ca90e0246855
aiacuone <aiacuone@users.noreply.github.com>
2021-02-19 13:55:45 +0000

    added if statement to useEffect

--- src/Draggable.js
+++ src/Draggable.js
@@ -6,10 +6,10 @@ export default function Draggable() {
 
     const [pagePosition,setPagePosition]=useState({x:0,y:0})
     const [boxes, setBoxes]=useState({
-        1:{x:0,y:0},
-        2:{x:0,y:0},
-        3:{x:0,y:0},
-        4:{x:0,y:0},
+        1:{drag:true,x:0,y:0},
+        2:{drag:true,x:0,y:0},
+        3:{drag:true,x:0,y:0},
+        4:{drag:true,x:0,y:0},
     })
 
     function handleMouseOver(e){
@@ -18,29 +18,37 @@ export default function Draggable() {
         newPagePosition.y=e.pageY
         setPagePosition(newPagePosition)
     }
-        
 
-
-    function handleDrag(box){
+    function handleDrag(e,box){
+        e.preventDefault()
         const newBoxes={...boxes}
-        newBoxes[box].x=pagePosition.x
-        newBoxes[box].y=pagePosition.y
+        newBoxes[4].drag=true
         setBoxes(newBoxes)
-        // console.log('dragging')
     }
 
-    function handleDragEnd(){
-        // console.log('drag end')
+    function handleDragEnd(e,box){
+        e.preventDefault()
+        const newBoxes={...boxes}
+        newBoxes[4].drag=false
+        setBoxes(newBoxes)
     }
 
+    useEffect(()=>{
+        if(boxes[4].drag){
+            const newBoxes={...boxes}
+            newBoxes[4].x=pagePosition.x
+            newBoxes[4].y=pagePosition.y
+            setBoxes(newBoxes)
+        }
+    })
 
     return (
         <div class='draggable' onMouseMove={handleMouseOver}>
             {/* <div class='box 1'/>
             <div class='box 2'/>
             <div class='box 3'/> */}
-            <div class='box 4' style={{left:boxes[4].x,top:boxes[4].y}} onMouseDown={()=>handleDrag(4)} onDragEnd={()=>handleDragEnd(4)}/>
-            {/* {console.log(pagePosition)} */}
+            <div class='box 4' draggable='true' style={{left:boxes[4].x+'px',top:boxes[4].y+'px'}} onDrag={(e)=>handleDrag(e,4)} onDragEnd={(e)=>handleDragEnd(e,4)}/>
+
         </div>
     )
 }
