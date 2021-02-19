a757b251d26b2ac9de8323c81bd7091567f48728
aiacuone <aiacuone@users.noreply.github.com>
2021-02-19 12:58:18 +0000

    added drag boolean, included useEffect

--- src/Draggable.js
+++ src/Draggable.js
@@ -6,10 +6,10 @@ export default function Draggable() {
 
     const [pagePosition,setPagePosition]=useState({x:0,y:0})
     const [boxes, setBoxes]=useState({
-        1:{x:0,y:0},
-        2:{x:0,y:0},
-        3:{x:0,y:0},
-        4:{x:0,y:0},
+        1:{drag:false,x:0,y:0},
+        2:{drag:false,x:0,y:0},
+        3:{drag:false,x:0,y:0},
+        4:{drag:false,x:0,y:0},
     })
 
     function handleMouseOver(e){
@@ -18,20 +18,23 @@ export default function Draggable() {
         newPagePosition.y=e.pageY
         setPagePosition(newPagePosition)
     }
-        
 
-
-    function handleDrag(box){
+    function handleMouseDown(box){
         const newBoxes={...boxes}
-        newBoxes[box].x=pagePosition.x
-        newBoxes[box].y=pagePosition.y
+        newBoxes[box].drag=true
         setBoxes(newBoxes)
-        // console.log('dragging')
     }
 
-    function handleDragEnd(){
-        // console.log('drag end')
+    function handleMouseUp(box){
+        const newBoxes={...boxes}
+        newBoxes[box].drag=false
+        setBoxes(newBoxes)
     }
+
+    useEffect(()=>{
+        let dragging=document.getElementsByClassName('drag')
+        dragging.length>0&&console.log(dragging[0].style.left)
+    })
 
 
     return (
@@ -39,7 +42,7 @@ export default function Draggable() {
             {/* <div class='box 1'/>
             <div class='box 2'/>
             <div class='box 3'/> */}
-            <div class='box 4' style={{left:boxes[4].x,top:boxes[4].y}} onMouseDown={()=>handleDrag(4)} onDragEnd={()=>handleDragEnd(4)}/>
+            <div class={boxes[4].drag?'box 4 drag':'box 4'} style={{left:boxes[4].x+'px',top:boxes[4].y+'px'}} onMouseDown={()=>handleMouseDown(4)} onMouseUp={()=>handleMouseUp(4)}/>
             {/* {console.log(pagePosition)} */}
         </div>
     )
